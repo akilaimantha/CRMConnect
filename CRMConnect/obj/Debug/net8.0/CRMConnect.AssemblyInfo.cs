1617095c1967beba3310f342d4e2fa5ecbdf119c
akilaimantha <akilapcm2004@gmail.com>
2026-05-25 13:08:50 +0530

    .

--- CRMConnect/obj/Debug/net8.0/CRMConnect.AssemblyInfo.cs
+++ CRMConnect/obj/Debug/net8.0/CRMConnect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRMConnect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6611cb368c464225bee22a1f5062de813fdcb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b56693a35f4d34218a6ef58260215742fe3dd0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRMConnect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRMConnect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
